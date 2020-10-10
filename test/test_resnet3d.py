7da88674da86f14a8fe59805f7c3d06f44c6e71d
zjkjzj <wy163zhuj@163.com>
2020-10-10 19:59:45 +0800

    test(model): 测试resnet3d

--- test/test_resnet3d.py
+++ test/test_resnet3d.py
@@ -2,7 +2,7 @@
 
 """
 @date: 2020/9/28 下午8:59
-@file: resnet3d.py
+@file: test_resnet3d.py
 @author: zj
 @description: 
 """
@@ -12,15 +12,51 @@ import torch
 from tsn.model.backbones.resnet3d.build_resnet3d import resnet3d_50
 from tsn.config import cfg
 
-if __name__ == '__main__':
-    # cfg.merge_from_file('configs/c2d-nl_r3d50_ucf101_rgb_224x32_dense.yaml')
-    # cfg.merge_from_file('configs/c2d_r3d50_ucf101_rgb_224x32_dense.yaml')
-    cfg.merge_from_file('configs/i3d-3x1-nl_r3d50_ucf101_rgb_224x32_dense.yaml')
-    cfg.freeze()
 
-    model = resnet3d_50(cfg)
-    print(model)
-
+def test_c2d():
     data = torch.randn(1, 3, 32, 224, 224)
+
+    cfg.merge_from_file('configs/c2d-nl_r3d50_ucf101_rgb_224x32_dense.yaml')
+    model = resnet3d_50(cfg)
     outputs = model(data)
     print(outputs.shape)
+    assert outputs.shape == (1, 2048, 4, 7, 7)
+
+    cfg.merge_from_file('configs/c2d_r3d50_ucf101_rgb_224x32_dense.yaml')
+    model = resnet3d_50(cfg)
+    outputs = model(data)
+    print(outputs.shape)
+    assert outputs.shape == (1, 2048, 4, 7, 7)
+
+
+def test_i3d():
+    data = torch.randn(1, 3, 32, 224, 224)
+
+    cfg.merge_from_file('configs/i3d-3x1-nl_r3d50_ucf101_rgb_224x32_dense.yaml')
+    model = resnet3d_50(cfg)
+    outputs = model(data)
+    print(outputs.shape)
+    assert outputs.shape == (1, 2048, 4, 7, 7)
+
+    cfg.merge_from_file('configs/i3d-3x1_r3d50_ucf101_rgb_224x32_dense.yaml')
+    model = resnet3d_50(cfg)
+    outputs = model(data)
+    print(outputs.shape)
+    assert outputs.shape == (1, 2048, 4, 7, 7)
+
+    cfg.merge_from_file('configs/i3d-3x3-nl_r3d50_ucf101_rgb_224x32_dense.yaml')
+    model = resnet3d_50(cfg)
+    outputs = model(data)
+    print(outputs.shape)
+    assert outputs.shape == (1, 2048, 4, 7, 7)
+
+    cfg.merge_from_file('configs/i3d-3x3_r3d50_ucf101_rgb_224x32_dense.yaml')
+    model = resnet3d_50(cfg)
+    outputs = model(data)
+    print(outputs.shape)
+    assert outputs.shape == (1, 2048, 4, 7, 7)
+
+
+if __name__ == '__main__':
+    test_c2d()
+    test_i3d()
